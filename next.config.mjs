d8878fa5dcb9de727a7e8ec560f54af01ab40e11
mubarakwasim2007-hub <mubarakwasim2007@gmail.com>
2026-04-21 22:42:07 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  // This matches your repository name so CSS and Images load correctly
+  images: { 
+    unoptimized: true,
+  },
   basePath: '/v0-portfolio-website-design-ui',
   assetPrefix: '/v0-portfolio-website-design-ui',
+  trailingSlash: true, 
 };
 
 export default nextConfig;
